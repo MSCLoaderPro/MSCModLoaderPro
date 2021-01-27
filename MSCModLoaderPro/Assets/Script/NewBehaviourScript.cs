ed5dc560aeb0a881e5359d79b1cf45b0c4015049
Fredrik95 <fredrik.eriksson_95@live.se>
2021-01-27 16:18:02 +0100

    Upload Unity Project

---
+++ MSCModLoaderPro/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using MSCLoader;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public SettingKeybind keybind;
+
+    public Text down, up, held;
+
+    int downCount, upCount;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        held.text = keybind.GetKey().ToString();
+
+        if (keybind.GetKeyDown())
+        {
+            downCount++;
+            down.text = downCount.ToString();
+        }
+
+        if (keybind.GetKeyUp())
+        {
+            upCount++;
+            up.text = upCount.ToString();
+        }
+    }
+}
