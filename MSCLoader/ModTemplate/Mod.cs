061539a88498cc56caaed9f4322556d4b7452629
Athlon007 <aathlon@outlook.com>
2021-03-10 21:11:01 +0100

    Finished mod template and extension

--- MSCLoader/ModTemplate/Mod.cs
+++ MSCLoader/ModTemplate/Mod.cs
@@ -4,13 +4,15 @@ namespace $safeprojectname$
 {
 	public class $safeprojectname$ : Mod
     {
-        public override string ID => "MyMod";
-        public override string Name => "MY MOD";
+        public override string ID => "$safeprojectname$";
+        public override string Name => "$projectname$";
         public override string Author => "YOU";
         public override string Version => "1.0";
         public override string Description => "THIS IS MY AWESOME MOD!";
-        public override string UpdateLink => "https://www.nexusmods.com/mysummercar/mods/9999";
-        public override byte[] Icon => Properties.Resources.Icon;
+        // public override string UpdateLink => "https://www.nexusmods.com/mysummercar/mods/9999";
+        // public override byte[] Icon => Properties.Resources.Icon;
+
+        // Want to use those values? See Mod Loader Pro documentation!
 
         public override void ModSettings()
         {
